2597d6c7a14cfcf760f13652b95dd33623c77a08
Leanghok <leanghokoeng5@gmail.com>
2025-01-03 18:54:21 +0700

    adjust width of main

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div class="min-h-screen bg-[#303446] text-[#C6D0F5]">
-	<main class="mx-auto max-w-xl">
+	<main class="mx-auto max-w-2xl">
 		<h1 class="p-10 text-center text-4xl font-black text-[#8CAAEE]">Yapping Room</h1>
 		{@render children()}
 	</main>
