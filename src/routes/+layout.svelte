1d7cfdd6416efbe8b201183507a1c9a69c504421
leanghok <leanghokoeng5@gmail.com>
2025-01-06 17:19:08 +0700

    move everything from layout to page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,4 @@
 	let { children } = $props();
 </script>
 
-<div class="min-h-screen bg-[#303446] text-[#C6D0F5]">
-	<main class="mx-auto max-w-2xl">
-		<h1 class="p-10 text-center text-4xl font-black text-[#8CAAEE]">Yapping Room</h1>
-		{@render children()}
-	</main>
-</div>
+{@render children()}
