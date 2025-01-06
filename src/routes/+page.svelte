585c8a717c5d38662821826a4ffc90aef35bc47e
leanghok <leanghokoeng5@gmail.com>
2025-01-06 17:56:23 +0700

    finish basic style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,16 @@
-<main class="mx-auto max-w-2xl">
+<main class="mx-auto flex min-h-screen max-w-2xl flex-col">
 	<h1 class="p-10 text-center text-4xl font-black text-[#8CAAEE]">Yapping Room</h1>
-	<form>
+
+	<div class="flex-1 space-y-5">
+		<div>
+			<h2 class="font-bold">Anya</h2>
+			<div class="w-fit rounded-xl border-2 border-[#626880] bg-[#414559] px-6 py-3 text-center">
+				<p>I hate studying</p>
+			</div>
+		</div>
+	</div>
+
+	<form class="py-10">
 		<input
 			type="text"
 			placeholder="Type here..."
