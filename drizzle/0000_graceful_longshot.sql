968f0a1b743efde0c6627bfd27e4a7ce92469e78
leanghok <leanghokoeng5@gmail.com>
2025-01-07 16:38:06 +0700

    generate schema

---
+++ drizzle/0000_graceful_longshot.sql
@@ -0,0 +1,30 @@
+CREATE TABLE IF NOT EXISTS "message" (
+	"id" text PRIMARY KEY NOT NULL,
+	"content" text NOT NULL,
+	"user_name" text NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "session" (
+	"id" text PRIMARY KEY NOT NULL,
+	"user_id" text NOT NULL,
+	"expires_at" timestamp with time zone NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "user" (
+	"id" text PRIMARY KEY NOT NULL,
+	"username" text NOT NULL,
+	"password_hash" text NOT NULL,
+	CONSTRAINT "user_username_unique" UNIQUE("username")
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "message" ADD CONSTRAINT "message_user_name_user_username_fk" FOREIGN KEY ("user_name") REFERENCES "public"."user"("username") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "session" ADD CONSTRAINT "session_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
